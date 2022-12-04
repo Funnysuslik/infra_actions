b5a079e5e14e062d7a4b85b282b8e4360732d780
Funnysuslik <stepanov.step@gmail.com>
2022-12-04 16:03:49 +0300

    trying new ssh_key

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! test_1')
+    return HttpResponse('У меня получилось! test_2')
 
 
 def second_page(request):
